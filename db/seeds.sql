19a6f957920922280f50c2f02c4f70b71e71db9c
vanhooktristan <vanhooktristan@gmail.com>
2018-04-28 17:19:27 -0500

    importing app into github repo

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hot as Hell Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Melted Goat Cheese Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Triple Bacon Bun Burger", FALSE);
